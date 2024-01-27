9405b8cfe7295138abe2062410f5f864af2119c0
ShahirTukmen <shahirturkmen@gmail.com>
2024-01-27 11:01:54 +0430

    init

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ import { format } from 'date-fns'
 
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
+const datePickerHeaderText = document.querySelector('.current-month')
 
 datePickerButton.addEventListener('click', () => {
     datePicker.classList.toggle('show')
+    setupDatePicker(date)
 })
 
 function setDate(date) {
