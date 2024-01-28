13d0235a5c58c5dd6b2f324a2152e071b51679f6
ShahirTukmen <shahirturkmen@gmail.com>
2024-01-28 07:39:19 +0430

    init

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { format } from 'date-fns'
+import { format, fromUnixTime, getUnixTime } from 'date-fns'
 
 const datePickerButton = document.querySelector('.date-picker-button')
 const datePicker = document.querySelector('.date-picker')
@@ -6,11 +6,17 @@ const datePickerHeaderText = document.querySelector('.current-month')
 
 datePickerButton.addEventListener('click', () => {
     datePicker.classList.toggle('show')
-    setupDatePicker(date)
+    const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+    setupDatePicker(selectedDate)
 })
 
 function setDate(date) {
     datePickerButton.innerText = format(date, 'MMMM do, yyyy')
+    datePickerButton.dataset.selectedDate = getUnixTime(date)
+}
+
+function setupDatePicker(selectedDate) {
+    datePickerHeaderText.innerText = format(selectedDate, 'MMMM - yyyy')
 }
 
 setDate(new Date())
